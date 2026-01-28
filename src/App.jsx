229518c11822a2f054e09a81ebbe9bf46c8cba97
HendraSetiawan <165660625+Henzz2490@users.noreply.github.com>
2026-01-28 13:00:16 +0200

    moving project from other account

---
+++ src/App.jsx
@@ -0,0 +1,25 @@
+import React from 'react'
+import gsap from 'gsap'
+import { ScrollTrigger, SplitText } from 'gsap/all'
+
+import Navbar from './components/Navbar.jsx'  // Add this line
+import Hero from './components/Hero.jsx'
+import Cocktails from "./components/Cocktails.jsx";
+import About from "./components/About.jsx"
+
+
+gsap.registerPlugin(ScrollTrigger, SplitText);
+
+const App = () => {
+  return (
+    <main>
+        <Navbar />
+        <Hero />
+        <Cocktails />
+        <About />
+        <div className="h-dvh bg-black"></div>
+    </main>
+  )
+}
+
+export default App
